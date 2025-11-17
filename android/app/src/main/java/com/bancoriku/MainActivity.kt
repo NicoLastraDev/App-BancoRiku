01e62958f5d3e1d2f720d31bce66b8034d4432e2
Riku Okawa <nicolas.lastra.olguin@gmail.com>
2025-11-17 09:48:49 -0300

    se configura android manifest para que acepte conexion a internet y permisos especiales de red, se configura app.json para que pueda crear el apk sin problemas

--- android/app/src/main/java/com/bancoriku/MainActivity.kt
+++ android/app/src/main/java/com/bancoriku/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.bancoriku
+package com.bancoriku
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/bancoriku/MainApplication.kt
+++ android/app/src/main/java/com/bancoriku/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.bancoriku
+package com.bancoriku
 
 import android.app.Application
 import android.content.res.Configuration
